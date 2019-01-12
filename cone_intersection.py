fb33ca5916982113de71f28644b6cd8f2780ce53
bfmorri <44278735+bfmorri@users.noreply.github.com>
2019-01-12 11:27:40 -0500

    Add files via upload

--- cone_intersection.py
+++ cone_intersection.py
@@ -11,26 +11,32 @@ a = v_sound*dt/2
 
 b = ((.025**2-a**2)**(1/2))/2
 
+# a and b from acoustics pdf
+
 m = b/a
 
 n = b/a
 
+#m and n from wolfram alpha equation
+
 denom = (m**2+1)*n**2
-y_s = ((-m**2*n**2-1)/denom)**(1/2)
+y_s = ((m**2*n**2-1)/denom)**(1/2)
+# the original equation had a negative numerator creating a imaginary number as the answer
 z_s = ((m**2*(n**2+1)/denom)**(1/2))
+# something must be wrong because this reduces to just one
 
 
-print(y_s,"x")
-print(z_s,'x')
+print("the y slope is",y_s,"x")
+print('the z slope is',z_s,'x')
 
-x = np.arange(10)
-y = [(y_s * i) for i in x]
-z= [(z_s * i)for i in x]
-plt.plot(x,y)
-plt.plot(x,z)
-plt.show()
-plt.plot(x,z)
-plt.show()
+# x = np.arange(10)
+# y = [(y_s * i) for i in x]
+# z= [(z_s * i)for i in x]
+# plt.plot(x,y)
+# plt.plot(x,z)
+# plt.show()
+# plt.plot(x,z)
+# plt.show()
 
 
 
