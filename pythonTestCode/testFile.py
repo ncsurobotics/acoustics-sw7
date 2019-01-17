83bc28de39e33692976c8eb2fe9e6dda3e221a25
Amalan Iyengar <akiyeng2@ncsu.edu>
2019-01-16 19:49:31 -0500

    Added data folders

---
+++ pythonTestCode/testFile.py
@@ -0,0 +1,143 @@
+import sys
+import numpy as np
+from scipy.signal import butter, lfilter
+bottom_left = [-0.0125, 0, -0.0125] # Channel 3
+bottom_right = [0.0125, 0, -0.0125] # Channel 4
+top_left = [-0.0125, 0, 0.0125] # Channel 1
+top_right = [0.0125, 0, 0.0125] # Channel 2
+SPEED_OF_SOUND = 1484
+
+def angleOfArrival(hydrophones, tdoa, front=True):
+    distance = np.linalg.norm(np.array(hydrophones[1]) - np.array(hydrophones[0])) / 2
+    A = tdoa * SPEED_OF_SOUND / 2
+    B = np.sqrt(distance ** 2 - A ** 2)
+    return np.rad2deg(np.arctan2(-A, front * B))
+
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+def calculate_shift(ch1, ch2):
+    mid = int(len(ch1) / 2)
+    xcorr = np.correlate(ch1, ch2, "same")[mid - 32:mid + 32]
+    return np.argmax(xcorr) - 32
+
+import matplotlib.pyplot as plt
+def getYaws(path):
+
+
+    data = np.genfromtxt(path, delimiter=",")
+
+    yaw = data[4, 0]
+    fs = data[4, 1]
+    data = np.transpose(data[0:4, :])
+
+    lowcut = 15e3
+    highcut = 50e3
+
+
+    ch1 = data[:, 0]
+    ch2 = data[:, 1] / 2.5
+    ch3 = data[:, 2]
+    ch4 = data[:, 3]
+    mid = int(len(ch1) / 2)
+    fftfreq = np.fft.fftfreq(len(ch1), d=528e-9)
+    fft = 20*np.log(np.abs(np.fft.fft(ch1)))
+    plt.figure()
+    plt.plot(fftfreq[:mid] / 1000, fft[:mid])
+    plt.title("Filtered FFT")
+    plt.xlabel("Frequency (KHz)")
+    plt.ylabel("Power (dB)")
+    plt.show(block=False)
+
+    plt.figure()
+    plt.plot(fftfreq[:mid] / 1000, 20*np.log(np.abs(np.fft.fft(ch3)))[:mid])
+    plt.title("Unfiltered FFT")
+    plt.xlabel("Frequency (KHz)")
+    plt.ylabel("Power (dB)")
+    plt.show(block=False)
+
+
+    plt.figure()
+    plt.plot(ch1)
+    plt.plot(ch2)
+    plt.title("Channels 1 and 2")
+    plt.show(block=False)
+        
+    plt.figure()
+    plt.plot(ch3)
+    plt.plot(ch4)
+    plt.title("Channels 3 and 4")
+    plt.show(block=False)
+
+    
+    ch1_filtered = butter_bandpass_filter(ch1, lowcut, highcut, fs)
+    ch2_filtered = butter_bandpass_filter(ch2, lowcut, highcut, fs)
+    ch3_filtered = butter_bandpass_filter(ch3, lowcut, highcut, fs)
+    ch4_filtered = butter_bandpass_filter(ch4, lowcut, highcut, fs)
+
+    ch1=ch1_filtered
+    ch2=ch2_filtered
+    ch3 = ch3_filtered
+    ch4 = ch4_filtered
+
+    start = 1000
+    stop = 2100
+
+
+    if(np.max(ch1[start:stop]) < 2000):
+        return (0, 0)
+    
+
+    top_shift = calculate_shift(ch1[start:stop], ch2[start:stop])
+    bottom_shift = calculate_shift(ch3[start:stop], ch4[start:stop])
+
+    plt.figure()
+    plt.title(path)
+    plt.plot(ch1[start:stop])
+    plt.plot(ch2[start:stop])
+    plt.legend(["Channel 1", "Channel 2"])
+    plt.show(block=False)
+
+
+    plt.figure()
+    plt.title("3+4")
+    plt.plot(ch3[start:stop])
+    plt.plot(ch4[start:stop])
+    plt.legend(["Channel 3", "Channel 4"])
+    plt.show(block=False)
+
+
+    plt.figure()
+    plt.title("Channels 1 and 2 shifted")
+    plt.plot(np.roll(ch1, -top_shift)[start:stop])
+    plt.plot(ch2[start:stop])
+    plt.legend(["Channel 1", "Channel 2"])
+    plt.show(block=False)
+
+    plt.figure()
+    plt.title("Channels 3 and 4 shifted")
+    plt.plot(np.roll(ch3, -bottom_shift)[start:stop])
+    plt.plot(ch4[start:stop])
+    plt.legend(["Channel 3", "Channel 4"])
+    plt.show()
+
+    tdoa = top_shift * 528e-9
+    print((top_shift, bottom_shift))
+    print(angleOfArrival([top_left, top_right], tdoa))
+    tdoa = bottom_shift * 528e-9
+    print(angleOfArrival([bottom_left, bottom_right], tdoa))
+    return (0, 0)
+
+getYaws(sys.argv[1])
--- pythonTestCode/testFolder.py
+++ pythonTestCode/testFolder.py
@@ -4,30 +4,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-def calcOrientation(tdoa):
 
-
-    SPEED_OF_SOUND = 1484
-    sideToSideDistance = np.linalg.norm(hydrophones[3] - hydrophones[2]) / 2
-    inlineDistance = np.linalg.norm(hydrophones[1] - hydrophones[0]) / 2 
-
-    sideToSideA = tdoa[1] * SPEED_OF_SOUND / 2
-    sideToSideB = np.sqrt(sideToSideDistance ** 2 - sideToSideA ** 2)
-
-    inlineA = tdoa[0] * SPEED_OF_SOUND / 2
-    inlineB = np.sqrt(inlineDistance ** 2 - inlineA ** 2)
-
-    front = np.sign(inlineA)
-    if front == 0:
-        front = 1
-    yaw = np.arctan2(-sideToSideA, front * sideToSideB)
-    pitch = np.arctan2(inlineA, inlineB)
-
-    return (np.rad2deg(yaw), np.rad2deg(pitch))
-
+bottom_left = [-0.0125, 0, -0.0125] # Channel 3
+bottom_right = [0.0125, 0, -0.0125] # Channel 4
+top_left = [-0.0125, 0, 0.0125] # Channel 1
+top_right = [0.0125, 0, 0.0125] # Channel 2
+SPEED_OF_SOUND = 1484
 
 def angleOfArrival(hydrophones, tdoa, front=True):
-    distance = np.linalg.norm(hydrophones[1] - hydrophones[0]) / 2
+    distance = np.linalg.norm(np.array(hydrophones[1]) - np.array(hydrophones[0])) / 2
     A = tdoa * SPEED_OF_SOUND / 2
     B = np.sqrt(distance ** 2 - A ** 2)
     return np.rad2deg(np.arctan2(-A, front * B))
@@ -48,11 +33,14 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
     y = lfilter(b, a, data)
     return y
 def calculate_shift(ch1, ch2):
-    return np.argmax(np.correlate(ch1, ch2, "same")) - int(len(ch1) / 2)
+    mid = int(len(ch1) / 2)
+    xcorr = np.correlate(ch1, ch2, "same")[mid - 32:mid + 32]
+    return np.argmax(xcorr) - 32
 
 path = sys.argv[1]
 
 def getYaws(path):
+
 
     data = np.genfromtxt(path, delimiter=",")
 
@@ -69,21 +57,69 @@ def getYaws(path):
     ch3 = data[:, 2]
     ch4 = data[:, 3]
 
-    bottom_left = [-0.0125, 0, -0.0125] # Channel 3
-    bottom_right = [0.0125, 0, -0.0125] # Channel 4
-    top_left = [-0.0125, 0, 0.0125] # Channel 1
-    top_right = [0.0125, 0, 0.0125] # Channel 2
-
+    fftfreq = np.fft.fftfreq(len(ch1), d=528e-9)
+    fft = 20*np.log(np.abs(np.fft.fft(ch1)))
+    #plt.plot(fftfreq, fft)
+    #plt.show()
+    
     ch1_filtered = butter_bandpass_filter(ch1, lowcut, highcut, fs)
     ch2_filtered = butter_bandpass_filter(ch2, lowcut, highcut, fs)
     ch3_filtered = butter_bandpass_filter(ch3, lowcut, highcut, fs)
     ch4_filtered = butter_bandpass_filter(ch4, lowcut, highcut, fs)
 
+    ch1=ch1_filtered
+    ch2=ch2_filtered
+    ch3 = ch3_filtered
+    ch4 = ch4_filtered
 
-    top_shift = calculate_shift(ch1_filtered[1800:3000], ch2_filtered[1800:3000])
-    bottom_shift = calculate_shift(ch4_filtered[1800:3000], ch3_filtered[1800:3000])
+    start = 1000
+    stop = 2100
 
-    return (top_shift, bottom_shift)
+
+    if(np.max(ch1[start:stop]) < 2000):
+        return (0, 0)
+    
+
+    top_shift = calculate_shift(ch1[start:stop], ch2[start:stop])
+    bottom_shift = calculate_shift(ch3[start:stop], ch4[start:stop])
+    '''
+    plt.figure()
+    plt.title(path)
+    plt.plot(ch1[start:stop])
+    plt.plot(ch2[start:stop])
+    plt.legend(["Channel 1", "Channel 2"])
+    plt.show(block=False)
+
+
+    plt.figure()
+    plt.title("3+4")
+    plt.plot(ch3[start:stop])
+    plt.plot(ch4[start:stop])
+    plt.legend(["Channel 3", "Channel 4"])
+    plt.show(block=False)
+
+
+    plt.figure()
+    plt.title("Channels 1 and 2 shifted")
+    plt.plot(np.roll(ch1, -top_shift)[start:stop])
+    plt.plot(ch2[start:stop])
+    plt.legend(["Channel 1", "Channel 2"])
+    plt.show(block=False)
+
+    plt.figure()
+    plt.title("Channels 3 and 4 shifted")
+    plt.plot(np.roll(ch3, -bottom_shift)[start:stop])
+    plt.plot(ch4[start:stop])
+    plt.legend(["Channel 3", "Channel 4"])
+    plt.show()
+    '''
+    tdoa = top_shift * 528e-9
+    print((top_shift, bottom_shift))
+    print(angleOfArrival([top_left, top_right], tdoa))
+    tdoa = bottom_shift * 528e-9
+    print(angleOfArrival([bottom_left, bottom_right], tdoa))
+    return (0, 0)
+
 
 
 
@@ -91,12 +127,16 @@ files = os.listdir(path)
 tops = []
 bottoms = []
 for file in files:
+
+
+    
     top, bottom = getYaws(path + file)
+    '''
     if(np.abs(top) < 32):
         tops.append(top)
     if(np.abs(bottom) < 32):
         bottoms.append(bottom)
-
+    '''
 
 
 print(tops)
