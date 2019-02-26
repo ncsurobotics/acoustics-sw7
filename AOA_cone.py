e624a0eeb8b414c64b8189f09d6a443b46fcb590
bfmorri <44278735+bfmorri@users.noreply.github.com>
2019-02-25 21:29:09 -0500

    Update AOA_cone.py

--- AOA_cone.py
+++ AOA_cone.py
@@ -18,13 +18,13 @@ c2 = d2**2 / (hydrophone_spacing**2 - d2**2)
 # calculate yaw and pitch
 
 yaw = math.degrees(math.atan(math.sqrt((c1 + c1 * c2)/(1 - c1 * c2))))
-pitch = math.degrees(math.atan(math.sqrt((c1 + 1)/(c2 + c1 * c2))))
+pitch = 90 - math.degrees(math.atan(math.sqrt((c1 + 1)/(c2 + c1 * c2))))
 
 print("cone yaw: ", yaw)
 print("cone pitch: ", pitch)
 
 # test
-print(math.degrees(np.arctan(sim.pinger_locs[0, 1]/sim.pinger_locs[0, 0])), "yaw")
+print(math.degrees(np.arctan(sim.pinger_locs[0, 0]/sim.pinger_locs[0, 1])), "yaw")
 print(math.degrees(np.arctan2(sim.pinger_locs[0, 2], math.sqrt(sim.pinger_locs[0, 1]**2 + sim.pinger_locs[0, 0]**2))), "pitch")
 
 
